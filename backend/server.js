f95d919ad5ad95489dbad72e2bd4a8414847d623
Mitch Lillie <mitch@xponentialworks.com>
2017-07-06 12:53:45 -0700

    clean up and use standard

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,33 @@
-'use strict';
+'use strict'
 
 const GITLAB_URL = process.env.GITLAB_PORT_80_TCP_ADDR || 'http://localhost'
 
-const express = require('express');
-const nodegit = require('nodegit');
-var promisify = require("promisify-node")
+const promisify = require('promisify-node')
+
+const express = require('express')
 const fse = promisify(require('fs-extra'))
-const path = require('path')
-fse.ensureDir = promisify(fse.ensureDir);
+fse.ensureDir = promisify(fse.ensureDir)
 const gitlab = require('gitlab')({
-  url:   'http://172.17.0.4',
+  url: 'http://172.17.0.4',
   token: 'HtZjA6YjYjx85V1Vs8MV'
-});
+})
+const nodegit = require('nodegit')
+const path = require('path')
 const winston = require('winston')
 
 // Constants
-const PORT = 8888;
-const HOST = '0.0.0.0';
-const TMP_DIR = '../git-tmp' //TODO change to something better like /git-tmp
+const PORT = 8888
+const HOST = '0.0.0.0'
+const TMP_DIR = '../git-tmp' // TODO change to something better like /git-tmp
 
-var repository;
-var index;
-var remote;
+var repository
+var index
+var remote
 
 // App
-const app = express();
+const app = express()
 app.get('/', function (req, res) {
-  res.send('Hello worlds!\nIt can\'t be!');
-});
-
-app.get('/testing', function (req, res) {
-
-  // gitlab.users.all(function(users) {
-  //   for (var i = 0; i < users.length; i++) {
-  //     console.log("#" + users[i].id + ": " + users[i].email + ", " + users[i].name + ", " + users[i].created_at);
-  //   }
-  // });
+  res.send('Hello worlds!\nIt can\'t be!')
 })
 
 app.get('/repos/:id', function (req, res) {
@@ -43,7 +35,7 @@ app.get('/repos/:id', function (req, res) {
   let tmpRepoPath = path.resolve(TMP_DIR, id)
 
   fse.ensureDir(tmpRepoPath)
-    .then(function() {
+    .then(function () {
       return nodegit.Config.openDefault().then(function (config) {
         return Promise.all([
           config.setString('user.name', 'Administrator'),
@@ -51,76 +43,73 @@ app.get('/repos/:id', function (req, res) {
         ])
       })
     })
-    .then(function() {
-      return nodegit.Repository.init(tmpRepoPath, 0);
+    .then(function () {
+      return nodegit.Repository.init(tmpRepoPath, 0)
     })
-    .then(function(repo) {
-      repository = repo;
-      return fse.writeFile(path.join(repository.workdir(), 'message.txt'), 'hello node.js');
+    .then(function (repo) {
+      repository = repo
+      return fse.writeFile(path.join(repository.workdir(), 'message.txt'), 'hello node.js')
     })
-    .then(function(){
-      return repository.refreshIndex();
+    .then(function () {
+      return repository.refreshIndex()
     })
-    .then(function(idx) {
-      index = idx;
+    .then(function (idx) {
+      index = idx
     })
-    .then(function() {
-      return index.addByPath('message.txt');
+    .then(function () {
+      return index.addByPath('message.txt')
     })
-    .then(function() {
-      return index.write();
+    .then(function () {
+      return index.write()
     })
-    .then(function() {
-      return index.writeTree();
+    .then(function () {
+      return index.writeTree()
     })
-    .then(function(oid) {
-      var author = nodegit.Signature.create("Git Answers",
-        "mlillie87@gmail.com", 123456789, 60);
-      var committer = nodegit.Signature.create("Git Answers",
-        "mlillie87@gmail.com", 987654321, 90);
+    .then(function (oid) {
+      var author = nodegit.Signature.create('Git Answers',
+        'mlillie87@gmail.com', 123456789, 60)
+      var committer = nodegit.Signature.create('Git Answers',
+        'mlillie87@gmail.com', 987654321, 90)
 
       // Since we're creating an inital commit, it has no parents. Note that unlike
       // normal we don't get the head either, because there isn't one yet.
-      return repository.createCommit("HEAD", author, committer, "message", oid, []);
+      return repository.createCommit('HEAD', author, committer, 'message', oid, [])
     })
-    .then(function() {
+    .then(function () {
       winston.debug('committed')
-      return nodegit.Remote.create(repository, "origin",
+      return nodegit.Remote.create(repository, 'origin',
         `git@${GITLAB_URL}:root/${id}.git`)
-      .then(function(remoteResult) {
+      .then(function (remoteResult) {
         winston.debug('remote created')
-        remote = remoteResult;
+        remote = remoteResult
 
-        return new Promise(function(resolve, reject) {
-          gitlab.projects.create({ name: `${id}`}, function (res) {
-            winston.debug('Created project: ', res);
+        return new Promise(function (resolve, reject) {
+          gitlab.projects.create({name: `${id}`}, function (res) {
+            winston.debug('Created project: ', res)
             // Create the push object for this remote
             remote.push(
-              ["refs/heads/master:refs/heads/master"],
+              ['refs/heads/master:refs/heads/master'],
               {
                 callbacks: {
-                  certificateCheck: function() { return 1; },
-                  credentials: function(url, userName) {
-                    return nodegit.Cred.sshKeyNew(userName, '/root/.ssh/id_rsa.pub', '/root/.ssh/id_rsa', '');
+                  certificateCheck: function () { return 1 },
+                  credentials: function (url, userName) {
+                    return nodegit.Cred.sshKeyNew(userName, '/root/.ssh/id_rsa.pub', '/root/.ssh/id_rsa', '')
                   }
                 }
               }
-            ).then(resolve).catch(reject);
+            ).then(resolve).catch(reject)
           })
-        });
-
-
-      });
+        })
+      })
     })
     .catch(err => {
       winston.error(err)
     })
-    .done(function() {
-      winston.debug("Done!");
-    });
-
+    .done(function () {
+      winston.debug('Done!')
+    })
 })
 
-app.listen(PORT, HOST);
-winston.info('Running on http://' + HOST + ':' + PORT);
-winston.debug('Listening with environment vars: ', process.env);
+app.listen(PORT, HOST)
+winston.info('Running on http://' + HOST + ':' + PORT)
+winston.debug('Listening with environment vars: ', process.env)
